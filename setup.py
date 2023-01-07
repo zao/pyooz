f27d04f6d9000654b79eb07a251a98e02a7136e3
Lars Viklund <zao@zao.se>
2023-01-07 05:05:59 +0100

    Bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='pyooz',
-    version='0.0.7',
+    version='0.0.8',
     description="ooz decompression bindings",
     long_description=long_description,
     long_description_content_type="text/markdown",
